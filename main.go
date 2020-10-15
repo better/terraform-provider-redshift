90a7b89e8c529440d493d1ebab54a230363548fc
Vitaly Sergeyev <vsergeyev@better.com>
2020-10-15 11:16:20 -0400

    import fix

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/better/terraform-provider-redshift"
+	"github.com/better/terraform-provider-redshift/redshift"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
